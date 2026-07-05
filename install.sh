c9eb8545c1827eebdcf83b3ab345ef4641435437
joshinc-uk <je6965534@gmail.com>
2026-07-05 14:09:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-echo "Installing JoshOS update A002..."
+(
+echo "10"
+echo "# Preparing update..."
+
+sleep 1
+
+echo "40"
+echo "# Installing files..."
+
+sleep 1
+
+echo "70"
+echo "# Applying system changes..."
 
 echo "JoshOS Alpha A002" > "$HOME/joshos-version.txt"
 
-echo "Update installed successfully"
+sleep 1
+
+echo "100"
+echo "# Finalising update..."
+
+sleep 1
+) | zenity --progress \
+--title="JoshOS Updater" \
+--text="Starting update..." \
+--percentage=0 \
+--auto-close \
+--no-cancel
+
+zenity --info --text="Update installed successfully ✅\nPlease restart JoshOS"
